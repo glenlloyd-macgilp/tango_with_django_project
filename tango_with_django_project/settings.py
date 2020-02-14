b545a9d3a7569fd1713ac11af1558e29c4735b20
Glen Lloyd-MacGilp <2394934l@student.gla.ac.uk>
2020-02-14 09:08:23 +0000

    passed chapter 10 tests, woo!

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -23,6 +23,8 @@ MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
 LOGIN_URL = 'rango:login'
 
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
